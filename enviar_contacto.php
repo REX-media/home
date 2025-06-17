51b8952c653ac599ae70faf22f747df4d02b16f7
RADrender <45047749+robertocarlosaranguiz@users.noreply.github.com>
2025-06-16 23:35:44 -0400

    Add files via upload

---
+++ enviar_contacto.php
@@ -0,0 +1,3 @@
+$SECRET_KEY = '6LeVdHgqAAAAADQFDGuPq04SU13TPJFIJUB2XQ0N'; // Aquí va tu Secret Key de reCAPTCHA
+$EMAIL_DESTINO = 'mauricio.lara@rex-media.cl'; 'contacto@rex-media.cl'; // Tu email
+$EMAIL_REMITENTE = 'contacto@rex-media.cl'; // Email del dominio
